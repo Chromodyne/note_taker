715192a2eb99c64b659a678ffa4c53653b7de576
chromodyne <dsinitiere@outlook.com>
2022-07-20 21:28:19 -0700

    Began working on saving note. Parsing old db and appending new to it.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//TODO: Add modular routing.
+//TODO: Add modular routing using notes.js in ./routes
 
 //Get the notes.html when requested.
 app.get("/notes", (req, res) => {
@@ -29,9 +29,39 @@ app.get("/api/notes", (req, res) => {
 
 //TODO: Save note routing.
 app.post("/api/notes", (req, res) => {
-    res.json(`Server received ${req.method} request for saving a note.`);
 
+    //TODO: We need to get the information passed in from the body and then write it to the db.json
+    //before we return a response.
+
+    res.json(`Server received ${req.method} request for saving a note.`);
     console.log(`Client successfully sent ${req.method} request for saving a note.`);
+
+    //DEBUGGING
+    console.log(req.body);
+
+    //Destructure request body to use keys.
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+        const newNote = {
+            title,
+            text
+        }
+
+        //TODO: Maybe change this to not be imported if doesn't work.
+        fs.readFile(db, "utf8", (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                
+            }
+        })
+
+    } else {
+
+    }
+    
 
 });
 
